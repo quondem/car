fc96a82d55fac2044a9da9d0eea3741114b6b268
Matvey <shul-m@mail.ru>
2023-07-10 08:04:47 +0300

    first commit

--- main.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector('.burger')
-const menu = document.querySelector('.menu')
-const body = document.querySelector('body')
-
-burger.addEventListener('click', function () {
-    menu.classList.toggle('lg:hidden')
-    body.classList.toggle('overflow-hidden')
-})
